3649372f272d4002299be90910350ec666194a2a
niudevelop <info@niudevelop.com>
2026-01-09 21:11:27 +0100

    cd workflow

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import pluginSecurity from "eslint-plugin-security"
 
 export default defineConfig([
   {
-    ignores: ["dist/**"],
+    ignores: ["dist/**", ".github/**"],
   },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
